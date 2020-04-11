8810a2f0111baf447cc1ffe8e52194bafaf1e8e5
영대 김 <zerobig.kim@gmail.com>
2020-04-11 08:57:59 +0000

    Added comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        // My first changes.
+        // My second changes.
         [Key]
         public int CartItemId { get; set; }
 
